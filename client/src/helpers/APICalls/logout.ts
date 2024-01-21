b43d637c6e3cfc7789c58d5ef0f444c858ceb471
ANKUR KUMAR <96306426+ankur-357@users.noreply.github.com>
2024-01-21 07:37:08 +0530

    Update logout.ts

--- client/src/helpers/APICalls/logout.ts
+++ client/src/helpers/APICalls/logout.ts
@@ -6,7 +6,7 @@ const logout = async (): Promise<AuthApiData> => {
     method: 'GET',
     credentials: 'include',
   };
-  return await fetch(`http://react-contest.onrender.com/auth/logout`, fetchOptions)
+  return await fetch(`https://react-contest.onrender.com/auth/logout`, fetchOptions)
     .then((res) => res.json())
     .catch(() => ({
       error: { message: 'Unable to connect to server. Please try again' },
